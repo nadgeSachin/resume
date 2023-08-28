06149d31f6886cfc1a2bfdcbe8ab31fbb313521c
Errorpat-404 <nadgesachin644@gmail.com>
2023-08-28 17:32:13 +0530

    latest commit

--- src/profile.js
+++ src/profile.js
@@ -63,7 +63,7 @@ const projects = [
     name: "PDF Toolbox",
     skills: ["Python, PyPDF2, Pillow, React, Flask, HTML/CSS"],
     about: "Created a software application using Python, PyPDF2, and Pillow that enables users to merge, split, and add images to PDFs. Designed a user-friendly interface(React) to enhance user experience and improve productivity when working with PDFs. ",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
     images: [
       require('./images/d1.png'),
       require('./images/d2.png'),
@@ -78,7 +78,7 @@ const projects = [
     name: "Algorithm Visualizer",
     skills: ["React, Node.js, Express, HTML/CSS, Algorithms"],
     about:"This software developer has created a data structure and algorithm visualizer using React and Node.js. The visualizer includes sorting algorithms and other algorithm visualizations that are easy to understand. The application simplifies complex concepts and helps users visualize algorithm behavior in real-time. ",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
     images: [
       require('./images/dsa.png'),
       require('./images/p1.png'),
@@ -92,14 +92,14 @@ const projects = [
     name: "Download Manager",
     skills: ["Java, JavaFX, Multithreading,Java Networking"],
     about:"Creating a download manager using JavaFX and multithreading allows multiple download links to be processed concurrently. This project involves creating a user interface, implementing multithreading, using the Java networking API, displaying progress updates, handling errors, and allowing the user to pause, resume, and cancel downloads.",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
   },
   {
     id: "project4",
     name: "Gmail Clone",
     skills: ["React, Node.js, MongoDB, MUI, Axios"],
     about: "This repository showcases a Gmail-clone crafted with React, MUI, and MongoDB. Designed responsively, it ensures optimal UX across devices. The application facilitates categorized email viewing like Inbox, Starred, and Bin. Users can star/unstar emails, manage drafts, and send or bin emails. The robust backend is developed using Node.js, Express, and MongoDB, supported by RESTful API endpoints for smooth email CRUD operations.",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
     images: [
       require('./images/p8.png'),
       require('./images/p3.png'),
@@ -111,7 +111,7 @@ const projects = [
     name: "WhatsApp Clone",
     skills: ["React, Node.js, Socket.io, MongoDB, Google OAuth"],
     about: "A robust WhatsApp clone built using the MERN stack, facilitating real-time chat functionalities. The app incorporates Google OAuth for authentication, and uses Socket.io to ensure instantaneous message delivery. The UI closely mirrors the original WhatsApp design, ensuring a familiar experience for users.",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
     images: [
       require('./images/p9.png'),
       require('./images/p10.png'),
@@ -125,22 +125,38 @@ const projects = [
     name: "Ecommerce Website",
     skills: ["HTML, CSS, JS"],
     about:"",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
   },
   {
     id: "project6",
     name: "Intagram Clone",
     skills: ["HTML, CSS, JS"],
     about:"",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
   },
   {
     id: "project6",
     name: "Blog Website",
     skills: ["HTML, CSS, JS"],
     about:"",
-    url: "https://github.com/kaustubhai",
+    url: "https://github.com/nadgesachin",
   },
+  {
+    id: "project6",
+    name: "Music Player",
+    skills: ["Java, Android Studio, XML"],
+    about:"Android App",
+    url: "https://github.com/nadgesachin",
+  },
+  {
+    "id": "project7",
+    "name": "Music Player",
+    "skills": ["Java", "Android Studio", "XML"],
+    "about": "An Android App designed to play music files from the user's device. It lists all available songs and provides playback controls such as play, pause, skip to next/previous, and repeat options (single, all).",
+    "url": "https://github.com/nadgesachin"
+  }
+  
+  
 ];
 const section4Title = "Miscellaneous";
 const miscellaneous = [
